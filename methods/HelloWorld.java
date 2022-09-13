9ede7e9ba6cd2f327cf985888f3d2c85be2b588c
Sheeshpal Singh <123sheeshpalsinghsaini@gmail.com>
2022-09-13 06:04:16 +0530

    updated

--- methods/HelloWorld.java
+++ methods/HelloWorld.java
@@ -1,6 +1,12 @@
 package methods;
 public class HelloWorld{
 	public static void main(String[]args){
+
 		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Good Morning ");
+		
 	}
 }
